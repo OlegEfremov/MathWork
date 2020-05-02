9fb3766674c55f94487301402a39a0a0e8d3cc7e
OlegEfremov <olegefremov70@mail.ru>
2020-05-02 10:31:31 +0300

    Отладка

--- apps/Export/views.py
+++ apps/Export/views.py
@@ -94,16 +94,16 @@ def phantomjs_to_pdf(request):
     except Exception as e:
         return HttpResponse("Ошибка path: " + str(e) + " path: "+ path)
 
-    aurl = request.build_absolute_uri(reverse("show_tasks_for_pdf"))
-    if aurl.find("127.0.0.1:8000") == -1 and aurl.find("localhost:8000")  == -1:
-        args = ["export", "QT_QPA_PLATFORM=offscreen"]
-
-        try:
+    try:
+        aurl = request.build_absolute_uri(reverse("show_tasks_for_pdf"))
+        if aurl.find("127.0.0.1:8000") == -1 and aurl.find("localhost:8000")  == -1:
+            args = ["export", "QT_QPA_PLATFORM=offscreen"]
             outcmd = subprocess.check_output(args)
-        except Exception as e:
-            return HttpResponse("Ошибка export: " + str(e) + " вывод " + outcmd)
-        else:
-            return HttpResponse("Все ОК: " + path)
+
+    except Exception as e:
+        return HttpResponse("Ошибка export: " + str(e) + " вывод " + outcmd)
+    else:
+        return HttpResponse("Все ОК: " + path)
 
 
 #    args = ["phantomjs", "/usr/share/doc/phantomjs/examples/rasterize.js",
