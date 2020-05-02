8d312d284ce5ab92b638103c3676c900b8a9abb5
OlegEfremov <olegefremov70@mail.ru>
2020-05-02 12:09:17 +0300

    Отладка

--- apps/Export/views.py
+++ apps/Export/views.py
@@ -91,8 +91,6 @@ def phantomjs_to_pdf(request):
     original_dir = os.getcwd()
     os.chdir(path)
 
-    outcmd = "null"
-
     args = ["phantomjs", "/usr/share/doc/phantomjs/examples/rasterize.js",
         request.build_absolute_uri(reverse("show_tasks_for_pdf"))+GET_param_str(request), path + filename]
 
@@ -107,7 +105,7 @@ def phantomjs_to_pdf(request):
     except Exception as e:
         outcmd = p.stdout.read()
         outerr = p.stderr.read()
-        return HttpResponse("Ошибка phantomjs: " + str(e) + " вывод: " + outcmd + " ошибка: " + outerr)
+        return HttpResponse("Ошибка phantomjs: " + str(e) + " вывод: " + str(outcmd) + " ошибка: " + str(outerr))
 
 
 # Показывает страницу со списком задач (для pdf экспорта)
